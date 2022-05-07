2a7539caaf1041728609d91e04ec9b2aa572f10f
Heber Sandoval <he3ers@gmail.com>
2022-05-07 19:43:11 -0400

    Able to fetch data from OpenWeather API and display it in the console.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,23 @@
 console.log('Weather App');
 
-// console.log(getAPIKey())
-
 const form = document.querySelector('.weather form');
-const input = document.querySelector('#city')
+const input = document.querySelector('#city');
+const msg = document.querySelector('.msg');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-
+    
+    const apiKey = getAPIKey();
     const inputValue = input.value
-    console.log(inputValue)
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=imperial`;
+    
+   fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Manipulate the data here
+            console.log(data);
+        })
+        .catch(() => {
+            msg.textContent = 'Please search for a valid city 😩 😔 🤨';
+        });
 });
