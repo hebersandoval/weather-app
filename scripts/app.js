38b0b5f0b2939beb4a542280fc170dc00e3859fb
Heber Sandoval <he3ers@gmail.com>
2022-05-07 19:13:02 -0400

    Able to grab value from form input programatically.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,13 @@
 console.log('Weather App');
 
 // console.log(getAPIKey())
+
+const form = document.querySelector('.weather form');
+const input = document.querySelector('#city')
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const inputValue = input.value
+    console.log(inputValue)
+});
